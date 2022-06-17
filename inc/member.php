6773654d11bd101ba6897fc6e2d51f1181cb6d45
Daniel Reintanz <85614916+FreelancerAMP@users.noreply.github.com>
2022-06-17 16:19:59 +0200

    CHANGES: UPDATED some member methods ADDED new member method get_member_profile_permalink

--- inc/member.php
+++ inc/member.php
@@ -22,8 +22,14 @@ class member extends stdClass
 
     }
 
+    public function get_member_profile_permalink()
+    {
+        wp_ulike_pro_get_user_profile_permalink($this->ID);
+    }
 
-    public function join_group($groupId)
+
+    public
+    function join_group($groupId)
     {
         if (add_post_meta($groupId, 'rpi_wall_member_id', $this->ID)
             && add_post_meta($this->ID, 'rpi_wall_group_id', $groupId)) {
@@ -35,7 +41,8 @@ class member extends stdClass
 
     }
 
-    public function leave_group($groupId)
+    public
+    function leave_group($groupId)
     {
 
         if (delete_post_meta($groupId, 'rpi_wall_member_id', $this->ID)
@@ -47,38 +54,49 @@ class member extends stdClass
 
     }
 
-    public function watch_group($groupid)
+    public
+    function watch_group($groupid)
     {
 
     }
 
-    public function get_groups()
+    public
+    function get_groups()
     {
         return get_post_meta($this->ID, 'rpi_wall_group_id');
     }
 
-    public function get_group($post_id)
+    public
+    function is_in_group($group_id): bool
     {
-
-        //TODO : Ist diese funktion notwendig?
-
+        $groups = get_post_meta($this->ID, 'rpi_wall_group_id');
+        return in_array($group_id, $groups);
     }
 
-    public function set_message_read($post_id)
-    {
-    }
-
-    public function get_messages()
+    public
+    function set_message_read($post_id)
     {
     }
 
-    public function current_user_is_member()
+    public
+    function get_messages()
     {
-
     }
 
-    public function current_member_can($capability)
+    public
+    function current_user_is_member()
     {
+        if (get_current_user() === $this->name) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public
+    function current_member_can($capability)
+    {
+        return user_can($this->ID, $capability);
     }
 
 }
