3e27f0f66572c991959b31739e3cdb3f74ea88c0
Daniel Reintanz <85614916+FreelancerAMP@users.noreply.github.com>
2022-06-17 15:26:44 +0200

    CHANGES: ADDED member class methods  WIP

--- inc/member.php
+++ inc/member.php
@@ -1,23 +1,84 @@
 <?php
+
 namespace rpi\Wall;
-class member {
 
-	public function join_group($groupId){
+class member extends stdClass
+{
 
-		add_post_meta($groupId, 'rpi_wall_member_id', $this->ID);
+    public $ID;
+    public $name;
+    public string $url;
+    public WP_Post $post;
 
-	}
-	public function leave_group($groupId){
+    public function __construct($post)
+    {
 
-		delete_post_meta($groupId, 'rpi_wall_member_id', $this->ID);
 
-	}
-	public function watch_group($post_id){}
-	public function get_groups(){}
-	public function get_group($post_id){}
-	public function set_message_read($post_id){}
-	public function get_messages(){}
-	public function current_user_is_member(){}
-	public function current_member_can($cability){}
+        $this->post = get_post($post, ARRAY_A);
+        $this->ID = $post->ID;
+        $this->name = $post->post_title;
+        $this->url = get_permalink($post->ID);
+
+
+    }
+
+
+    public function join_group($groupId)
+    {
+        if (add_post_meta($groupId, 'rpi_wall_member_id', $this->ID)
+            && add_post_meta($this->ID, 'rpi_wall_group_id', $groupId)) {
+            return true;
+        } else {
+            return false;
+        }
+
+
+    }
+
+    public function leave_group($groupId)
+    {
+
+        if (delete_post_meta($groupId, 'rpi_wall_member_id', $this->ID)
+            && delete_post_meta($this->ID, 'rpi_wall_group_id', $groupId)) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public function watch_group($groupid)
+    {
+
+    }
+
+    public function get_groups()
+    {
+        return get_post_meta($this->ID, 'rpi_wall_group_id');
+    }
+
+    public function get_group($post_id)
+    {
+
+        //TODO : Ist diese funktion notwendig?
+
+    }
+
+    public function set_message_read($post_id)
+    {
+    }
+
+    public function get_messages()
+    {
+    }
+
+    public function current_user_is_member()
+    {
+
+    }
+
+    public function current_member_can($capability)
+    {
+    }
 
 }
