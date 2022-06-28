8025bf99a2870edd8bdeb877bfe1c2bc6fb8c1c8
Daniel Reintanz <85614916+FreelancerAMP@users.noreply.github.com>
2022-06-28 09:20:42 +0200

    CHANGES: CHANGED CSS for better responsiveness ADDED new function to mark messages as read LINTED member.php

--- inc/member.php
+++ inc/member.php
@@ -12,35 +12,35 @@ class member extends \stdClass
     public $post;  //CPT member WP_Post
     public $user;
 
-	/**
-	 * @param WP_User|int $user
-	 */
+    /**
+     * @param WP_User|int $user
+     */
     public function __construct($user = 0)
     {
 
 
-	    if(is_a($user, 'WP_User')){
-		    $this->user = $user;
-		    $this->ID = $user->ID;
+        if (is_a($user, 'WP_User')) {
+            $this->user = $user;
+            $this->ID = $user->ID;
 
 
-	    }else{
-		    if ($user === 0) {
-			    $this->ID = get_current_user_id();
-		    }else{
-			    $this->ID = $user;
-		    }
+        } else {
+            if ($user === 0) {
+                $this->ID = get_current_user_id();
+            } else {
+                $this->ID = $user;
+            }
 
-		    $this->user = get_userdata($this->ID);
+            $this->user = get_userdata($this->ID);
 
-		    if(!$this->user){
-				return new \WP_Error('404','User not found');
-			}
-	    }
+            if (!$this->user) {
+                return new \WP_Error('404', 'User not found');
+            }
+        }
 
-	    $this->name = $this->user->display_name;
+        $this->name = $this->user->display_name;
 
-	    $this->url = $this->get_member_profile_permalink();
+        $this->url = $this->get_member_profile_permalink();
 
 
         $posts = get_posts(array(
@@ -50,12 +50,11 @@ class member extends \stdClass
         ));
 
 
-
-		if(count($posts)>0){
-			$this->post = reset($posts);
-		}else{
-			$this->post = $this->setup();
-		}
+        if (count($posts) > 0) {
+            $this->post = reset($posts);
+        } else {
+            $this->post = $this->setup();
+        }
 
 
     }
@@ -67,184 +66,203 @@ class member extends \stdClass
 
     }
 
-	public function get_liked_group_Ids(){
-		$ids = get_user_meta($this->ID, 'rpi_wall_liked_group_id');
+    public function get_liked_group_Ids()
+    {
+        $ids = get_user_meta($this->ID, 'rpi_wall_liked_group_id');
 
-		if(is_array($ids)){
-			return get_user_meta($this->ID, 'rpi_wall_liked_group_id');
-		}
-		return array();
+        if (is_array($ids)) {
+            return get_user_meta($this->ID, 'rpi_wall_liked_group_id');
+        }
+        return array();
 
-	}
-	public function is_liked_group($groupId){
-		return in_array($groupId, $this->get_liked_group_Ids());
-	}
+    }
 
-	public function like_group($groupId){
+    public function is_liked_group($groupId)
+    {
+        return in_array($groupId, $this->get_liked_group_Ids());
+    }
 
-		if(!$this->is_liked_group($groupId)){
-			$this->toggle_like_group($groupId);
-		}
-	}
-	public function un_like_group($groupId){
+    public function like_group($groupId)
+    {
 
-		if($this->is_liked_group($groupId)){
-			$this->toggle_like_group($groupId);
-		}
-	}
+        if (!$this->is_liked_group($groupId)) {
+            $this->toggle_like_group($groupId);
+        }
+    }
 
+    public function un_like_group($groupId)
+    {
 
-	public function toggle_like_group($groupId)
-	{
-		if($this->ID > 0){
-			if (!$this->is_liked_group($groupId)) {
-				add_post_meta($groupId, 'rpi_wall_liker_id', $this->ID);
-				add_user_meta($this->ID, 'rpi_wall_liked_group_id', $groupId);
-				$action = 'like';
-			}else{
-				delete_post_meta($groupId, 'rpi_wall_liker_id', $this->ID);
-				delete_user_meta($this->ID, 'rpi_wall_liked_group_id', $groupId);
-				$action = 'unlike';
-			}
-			//recalc likers_amount
-			if(!$ids = get_post_meta($groupId, 'rpi_wall_liker_id')){
-				$ids = [];
-			}
-			update_post_meta($groupId, 'rpi_wall_likers_amount', count($ids));
-
-			do_action('rpi_wall_member_joined_group',$this->ID, $groupId, $action);
-		}
+        if ($this->is_liked_group($groupId)) {
+            $this->toggle_like_group($groupId);
+        }
+    }
 
 
+    public function toggle_like_group($groupId)
+    {
+        if ($this->ID > 0) {
+            if (!$this->is_liked_group($groupId)) {
+                add_post_meta($groupId, 'rpi_wall_liker_id', $this->ID);
+                add_user_meta($this->ID, 'rpi_wall_liked_group_id', $groupId);
+                $action = 'like';
+            } else {
+                delete_post_meta($groupId, 'rpi_wall_liker_id', $this->ID);
+                delete_user_meta($this->ID, 'rpi_wall_liked_group_id', $groupId);
+                $action = 'unlike';
+            }
+            //recalc likers_amount
+            if (!$ids = get_post_meta($groupId, 'rpi_wall_liker_id')) {
+                $ids = [];
+            }
+            update_post_meta($groupId, 'rpi_wall_likers_amount', count($ids));
 
-	}
-	public function _toggle_like_group($groupId){
+            do_action('rpi_wall_member_joined_group', $this->ID, $groupId, $action);
+        }
 
-		$process  = new \wp_ulike_cta_process( array(
-			'item_id'       =>  $groupId,
-			'item_type'     =>  'post',
-			'item_template' =>  "wpulike-heart",
-			'user_id'       =>  $this->ID
-		) );
-		//toggle like/unlike
-		$process->update();
-	}
 
-	public function is_in_group_or_likes_group($groupId){
-		return in_array($groupId,get_assigned_group_Ids());
-	}
-	public function get_watched_group_Ids(){
-		return [];
-	}
-	public function get_assigned_group_Ids(){
+    }
 
-		$ids = array_merge($this->get_group_Ids(),$this->get_liked_group_Ids());
-		return $ids;
-	}
+    public function _toggle_like_group($groupId)
+    {
 
-	public function get_query_all_groups($args = array()){
+        $process = new \wp_ulike_cta_process(array(
+            'item_id' => $groupId,
+            'item_type' => 'post',
+            'item_template' => "wpulike-heart",
+            'user_id' => $this->ID
+        ));
+        //toggle like/unlike
+        $process->update();
+    }
 
-		if(!empty($postids= $this->get_assigned_group_Ids())){
-			$args = wp_parse_args($args,
-				[
-					'post_type' => 'wall',
-					'post__in' => $postids
-				]);
+    public function is_in_group_or_likes_group($groupId)
+    {
+        return in_array($groupId, get_assigned_group_Ids());
+    }
 
-			$query = new \WP_Query($args);
+    public function get_watched_group_Ids()
+    {
+        return [];
+    }
 
-			return $query;
-		}
-		return false;
-	}
+    public function get_assigned_group_Ids()
+    {
 
-	public function get_query_watched_groups($args = array()){
+        $ids = array_merge($this->get_group_Ids(), $this->get_liked_group_Ids());
+        return $ids;
+    }
 
-		if(!empty($postids= $this->get_watched_group_Ids())){
-			$args = wp_parse_args($args,
-				[
-					'post_type' => 'wall',
-					'post__in' => $postids
-				]);
+    public function get_query_all_groups($args = array())
+    {
 
-			$query = new \WP_Query($args);
-			return $query;
+        if (!empty($postids = $this->get_assigned_group_Ids())) {
+            $args = wp_parse_args($args,
+                [
+                    'post_type' => 'wall',
+                    'post__in' => $postids
+                ]);
 
-		}
-		return false;
+            $query = new \WP_Query($args);
 
-	}
+            return $query;
+        }
+        return false;
+    }
 
-	public function get_query_pending_groups($stati = array ('pending')){
-		if(!empty($postids= $this->get_assigned_group_Ids())){
-			$query = new \WP_Query([
-				'post_type' => 'wall',
-				'post__in' => $postids,
-				'meta_query'=>[
-					'key' => 'rpi_wall_group_status',
-					'value' => $stati,
-					'compare' => 'IN'
-				]
-			]);
-			return $query;
-		}
-		return false;
-	}
-	public function get_query_my_posts($args = array()){
-		$args = wp_parse_args($args,[
-			'post_type' => 'wall',
-			'post_author'=>$this->ID
-		]);
-		$query = new \WP_Query($args);
-		return $query;
-	}
-	public function get_my_comments_query($args = array()){
+    public function get_query_watched_groups($args = array())
+    {
 
-		$props = wp_parse_args($args,[
-			'author__in'=>[$this->ID]
-		]);
+        if (!empty($postids = $this->get_watched_group_Ids())) {
+            $args = wp_parse_args($args,
+                [
+                    'post_type' => 'wall',
+                    'post__in' => $postids
+                ]);
 
-		$comments_query = new \WP_Comment_Query( $props );
-		$comments = $comments_query->comments;
-		foreach ($comments as $key=>$comment){
-			$comments[$key]->post = get_post($comment->comment_post_ID);
-		}
-		return $comments;
-	}
+            $query = new \WP_Query($args);
+            return $query;
 
-	/**
-	 * helper function Mitglieder einer Gruppe sollen nicht zugleich liker sein
-	 * @return void
-	 */
-	public function clean_likes(){
+        }
+        return false;
 
-		foreach ($this->get_group_Ids() as $group_id){
-			if($this->is_liked_group($group_id)){
-				$this->un_like_group($group_id);
-			}
-		}
-	}
+    }
 
+    public function get_query_pending_groups($stati = array('pending'))
+    {
+        if (!empty($postids = $this->get_assigned_group_Ids())) {
+            $query = new \WP_Query([
+                'post_type' => 'wall',
+                'post__in' => $postids,
+                'meta_query' => [
+                    'key' => 'rpi_wall_group_status',
+                    'value' => $stati,
+                    'compare' => 'IN'
+                ]
+            ]);
+            return $query;
+        }
+        return false;
+    }
+
+    public function get_query_my_posts($args = array())
+    {
+        $args = wp_parse_args($args, [
+            'post_type' => 'wall',
+            'post_author' => $this->ID
+        ]);
+        $query = new \WP_Query($args);
+        return $query;
+    }
+
+    public function get_my_comments_query($args = array())
+    {
+
+        $props = wp_parse_args($args, [
+            'author__in' => [$this->ID]
+        ]);
+
+        $comments_query = new \WP_Comment_Query($props);
+        $comments = $comments_query->comments;
+        foreach ($comments as $key => $comment) {
+            $comments[$key]->post = get_post($comment->comment_post_ID);
+        }
+        return $comments;
+    }
+
+    /**
+     * helper function Mitglieder einer Gruppe sollen nicht zugleich liker sein
+     * @return void
+     */
+    public function clean_likes()
+    {
+
+        foreach ($this->get_group_Ids() as $group_id) {
+            if ($this->is_liked_group($group_id)) {
+                $this->un_like_group($group_id);
+            }
+        }
+    }
 
 
     public function join_group($groupId)
     {
-	    if ($this->is_in_group($groupId) || $this->ID < 1 ){
+        if ($this->is_in_group($groupId) || $this->ID < 1) {
             return false;
         }
         add_post_meta($groupId, 'rpi_wall_member_id', $this->ID);
         add_user_meta($this->ID, 'rpi_wall_group_id', $groupId);
 
-		$this->un_like_group($groupId);
+        $this->un_like_group($groupId);
 
-		do_action('rpi_wall_member_joined_group',$this->ID, $groupId);
+        do_action('rpi_wall_member_joined_group', $this->ID, $groupId);
     }
 
     public function leave_group($groupId)
     {
         delete_post_meta($groupId, 'rpi_wall_member_id', $this->ID);
-		delete_user_meta($this->ID, 'rpi_wall_group_id', $groupId);
-	    do_action('rpi_wall_member_left_group', $this, $groupId);
+        delete_user_meta($this->ID, 'rpi_wall_group_id', $groupId);
+        do_action('rpi_wall_member_left_group', $this, $groupId);
         return true;
 
     }
@@ -257,9 +275,9 @@ class member extends \stdClass
 
     public function get_group_Ids()
     {
-	    if(!$ids = get_user_meta($this->ID, 'rpi_wall_group_id')){
-		    return [];
-	    }
+        if (!$ids = get_user_meta($this->ID, 'rpi_wall_group_id')) {
+            return [];
+        }
         return $ids;
     }
 
@@ -267,12 +285,24 @@ class member extends \stdClass
     function is_in_group($group_id): bool
     {
         $groups = (array)get_user_meta($this->ID, 'rpi_wall_group_id');
-		return in_array($group_id, $groups);
+        return in_array($group_id, $groups);
     }
 
-    public
-    function set_message_read($post_id)
+    public function set_message_read($message_id)
     {
+        $read_messages = get_post_meta($this->ID, 'rpi_read_messages');
+        $read_messages[] = $message_id;
+        update_post_meta($this->ID, 'rpi_read_messages', $read_messages);
+    }
+
+    public function set_message_unread($message_id)
+    {
+        $read_messages = get_post_meta($this->ID, 'rpi_read_messages');
+        if (in_array($message_id, $read_messages)) {
+            $read_messages = array_diff($read_messages, $message_id);
+            update_post_meta($this->ID, 'rpi_read_messages', $read_messages);
+        }
+
     }
 
     public function get_messages()
@@ -320,11 +350,11 @@ class member extends \stdClass
                 $member = new member(intval($_REQUEST['new_group_member']));
                 $groupId = $member->validate_and_join($_REQUEST['hash']);
 
-				if($groupId){
-					wp_redirect(get_permalink($groupId));
-				}else{
-					wp_redirect(home_url());
-				}
+                if ($groupId) {
+                    wp_redirect(get_permalink($groupId));
+                } else {
+                    wp_redirect(home_url());
+                }
 
                 die();
 
@@ -365,7 +395,7 @@ class member extends \stdClass
 
         $groups = unserialize(get_user_meta($this->ID, 'rpi_wall_group_hash', true));
 
-        $hash = wp_hash($this->name.$group_id, 'nonce');
+        $hash = wp_hash($this->name . $group_id, 'nonce');
 
         $groups[$group_id] = $hash;
 
@@ -390,25 +420,29 @@ class member extends \stdClass
 
     }
 
-	public function display_gravtar($size = 96){
-		echo get_avatar($this->ID,$size);
-	}
-	public function display($size = 15){
-		Shortcodes::display_user($this->ID,$size);
-	}
+    public function display_gravtar($size = 96)
+    {
+        echo get_avatar($this->ID, $size);
+    }
+
+    public function display($size = 15)
+    {
+        Shortcodes::display_user($this->ID, $size);
+    }
 
 
-	public function setup(){
+    public function setup()
+    {
 
-		if (is_a($this->user, 'WP_User') && $this->user->ID>0) {
-			$member = wp_insert_post(array(
-				'post_title' => $this->user->display_name,
-				'post_status' => 'publish',
-				'post_author' => $this->user->ID,
-				'post_type' => 'member'
-			));
-			return get_post($member);
+        if (is_a($this->user, 'WP_User') && $this->user->ID > 0) {
+            $member = wp_insert_post(array(
+                'post_title' => $this->user->display_name,
+                'post_status' => 'publish',
+                'post_author' => $this->user->ID,
+                'post_type' => 'member'
+            ));
+            return get_post($member);
 
-		}
-	}
+        }
+    }
 }
