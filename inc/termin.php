6ad31b08c2a739d0ed8ef1d19a71f10e5d1aba09
Joachim Happel <joachim.happel@gmail.com>
2023-01-19 16:45:53 +0100

    termine: display participiants list

--- inc/termin.php
+++ inc/termin.php
@@ -12,33 +12,36 @@ class termin {
 
 	public function display_participants(){
 
-		$termin = get_post();
+		if(is_singular('termin')){
+			$termin = get_post();
 
-		$members = array();
-		$guests = array();
+			$members = array();
+			$guests = array();
 
-		echo '<h3>Teilnehmer:innen Liste</h3>';
+			echo '<h3>Teilnehmer:innen Liste</h3>';
 
-		$participants = get_post_meta($termin->ID, 'rpi_wall_termin_participant');
-		foreach ($participants as $participant){
-			$members[]=intval($participant);
+			$participants = get_post_meta($termin->ID, 'rpi_wall_termin_participant');
+			foreach ($participants as $participant){
+				$members[]=intval($participant);
+			}
+			$participants = get_post_meta($termin->ID, 'rpi_wall_termin_guest');
+			$amount_guests = count($participants);
+
+			foreach ($members as $mId ){
+
+				$member = new Member($mId);
+				$member->display(96);
+
+
+			}
+			if($amount_guests==1){
+				echo '<hr>Eine Person, die nicht angemeldet war, hat außerdem teilgenommen';
+			}elseif($amount_guests>1){
+				echo '<hr>'.$amount_guests.' Personen, die nicht angemeldet waren, haben außerdem teilgenommen';
+			}
+
+
 		}
-		$participants = get_post_meta($termin->ID, 'rpi_wall_termin_guest');
-		$amount_guests = count($participants);
-
-		foreach ($members as $mId ){
-
-			$member = new Member($mId);
-			$member->display(96);
-
-
-		}
-		if($amount_guests==1){
-			echo '<hr>Eine Person, die nicht angemeldet war, hat außerdem teilgenommen';
-		}elseif($amount_guests>1){
-			echo '<hr>'.$amount_guests.' Personen, die nicht angemeldet waren, haben außerdem teilgenommen';
-		}
-
 
 	}
 }
